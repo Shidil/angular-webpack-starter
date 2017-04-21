7ec74f8355a0801b31645c65a23c58f3e06fc044
Shidil Eringa <shidil@live.com>
2017-04-21 12:05:15 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import {
+  RouterModule,
+  PreloadAllModules
+} from '@angular/router';
+
+import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NoContentComponent } from './no-content/no-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NoContentComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
   ],
   providers: [],
   bootstrap: [AppComponent]
